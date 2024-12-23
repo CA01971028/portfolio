2eb043dc19680bda8aa58e45d795bbd0ebf7e493
CA01971028 <CA01971028@st.kawahara.ac.jp>
2024-12-23 15:03:49 +0900

    備忘録サイト追加

--- test/src/app/components/Cards.tsx
+++ test/src/app/components/Cards.tsx
@@ -5,6 +5,7 @@ import hamball from '../img/hamboll.png'
 import Thread from '../img/Thread.png'
 import Image from 'next/image';
 import Tag from './Tag'
+import Qitta from '../img/qiita.png'
 
 const Cards = () => {
     return (
@@ -17,7 +18,7 @@ const Cards = () => {
                         <span>TETRIS</span>
                     </div>
                     <div className='flex-1'>
-                        １回生の時に三人チームで作成したものでブロックを消すことでスコアが上がるテトリスのようなゲームです！
+                        　１回生の時に三人チームで作成したものでブロックを消すことでスコアが上がるテトリスのようなゲームです！
                     </div>
                     <div className='text-2xl'>使用言語</div>
                     <Tag items={['HTML', 'CSS', 'JavaScript']} />
@@ -31,7 +32,7 @@ const Cards = () => {
                         <span>ORENGE & BLUE</span>
                     </div>
                     <div className='flex-1'>
-                        2回生の時に三人チームで作成したもので〇×ゲーム少し似たゲームで持ち駒が三種類あり駒の種類によって駒の上から自分の駒を置いたり、置かれたり、自分の駒なら取って再配置できるようなゲームです！
+                        　2回生の時に三人チームで作成したもので〇×ゲーム少し似たゲームで持ち駒が三種類あり駒の種類によって駒の上から自分の駒を置いたり、置かれたり、自分の駒なら取って再配置できるようなゲームです！
                     </div>
                     <div className='text-2xl'>使用言語</div>
                     <Tag items={['HTML', 'CSS', 'JavaScript']} />
@@ -43,7 +44,7 @@ const Cards = () => {
                         <span>TRIP SITE</span>
                     </div>
                     <div className='flex-1'>
-                        2回生の時に２人チームで作成したもので旅行サイトを作成しつつフレームワークの学習することを目標に作成。
+                        　2回生の時に２人チームで作成したもので旅行サイトを作成しつつフレームワークの学習することを目標に作成。
                     </div>
                     <div className='text-2xl'>使用言語：フレームワーク</div>
                     <Tag items={['HTML', 'CSS', 'JavaScript', 'Bootstrap']} />
@@ -55,7 +56,7 @@ const Cards = () => {
                         <span>HAM BALL</span>
                     </div>
                     <div className='flex-1'>
-                        3回生の時に２人チームで作成したもので同じハムスターを接触させることでスコアが上昇していくゲームを作成しました。特徴としては、mysqlでスコアを管理しているためランキングなども表示できる。
+                        　3回生の時に２人チームで作成したもので同じハムスターを接触させることでスコアが上昇していくゲームを作成しました。特徴としては、mysqlでスコアを管理しているためランキングなども表示できる。
                     </div>
                     <div className='text-2xl'>使用言語：フレームワーク</div>
                     <Tag items={['HTML', 'CSS', 'TypeScript', 'Python', 'React', 'Material-UI', 'mysql', 'Flask']} />
@@ -69,10 +70,23 @@ const Cards = () => {
                         <span>THREAD</span>
                     </div>
                     <div className='flex-1'>
-                        3回生の時に作成したものでスレッド形式のwebアプリ特徴としては各スレッドに対してコメントをすることができ、新しいスレッドを追加することもできる
+                        　3回生の時に作成したものでスレッド形式のwebアプリ特徴としては各スレッドに対してコメントをすることができ、新しいスレッドを追加することもできる
                     </div>
                     <div className='text-2xl'>使用言語：フレームワーク</div>
                     <Tag items={['HTML', 'CSS', 'TypeScript',  'React', 'Tailwind CSS','Azure']} />
+                </div>
+                <div className='w-96 h-auto bg-white mx-3 my-3 rounded-lg flex flex-col'>
+                    <a href="https://gentle-wave-09d109700.5.azurestaticapps.net">
+                    <Image src={Qitta} alt="備忘録サイトの画像" className='h-52 mx-auto rounded-lg' />
+                    </a>
+                    <div className='text-4xl text-center font-script'>
+                        <span>memorider</span>
+                    </div>
+                    <div className='flex-1'>
+                        　3回生の時に作成した作品。３人チームで作成し、私はバックエンド・フロントエンドのフルスタックとしての開発を担当した。ログインでのセッション機能やいいね機能を導入がポイント。また、Azureを利用した作品でもあり、クラウド技術にも精通している。
+                    </div>
+                    <div className='text-2xl'>使用言語：フレームワーク</div>
+                    <Tag items={['HTML', 'CSS', 'TypeScript',  'Next.js', 'Tailwind CSS','Azure','Vercel']} />
                 </div>
             </div>
         </>
