5be927277d98c1808be26700d0974e25c170bd3a
CA01971028 <CA01971028@st.kawahara.ac.jp>
2024-12-31 13:30:00 +0900

    コード公開を表示

--- test/src/app/components/Cards.tsx
+++ test/src/app/components/Cards.tsx
@@ -20,6 +20,10 @@ const Cards = () => {
                     <div className='flex-1'>
                         　１回生の時に三人チームで作成したものでブロックを消すことでスコアが上がるテトリスのようなゲームです！
                     </div>
+                    <div>
+                        <div className='text-2xl'>code</div>
+                        <a className='hover:text-blue-600' href="https://github.com/CA01971028/tetorisu_group">Git hubコード</a>
+                    </div>
                     <div className='text-2xl'>使用言語</div>
                     <Tag items={['HTML', 'CSS', 'JavaScript']} />
                 </div>
@@ -34,6 +38,10 @@ const Cards = () => {
                     <div className='flex-1'>
                         　2回生の時に三人チームで作成したもので〇×ゲーム少し似たゲームで持ち駒が三種類あり駒の種類によって駒の上から自分の駒を置いたり、置かれたり、自分の駒なら取って再配置できるようなゲームです！
                     </div>
+                    <div>
+                        <div className='text-2xl'>code</div>
+                        <a className='hover:text-blue-600' href="https://github.com/CA01971028/orange_and_blue">Git hubコード</a>
+                    </div>
                     <div className='text-2xl'>使用言語</div>
                     <Tag items={['HTML', 'CSS', 'JavaScript']} />
                 </div>
@@ -45,6 +53,10 @@ const Cards = () => {
                     </div>
                     <div className='flex-1'>
                         　2回生の時に２人チームで作成したもので旅行サイトを作成しつつフレームワークの学習することを目標に作成。
+                    </div>
+                    <div>
+                        <div className='text-2xl'>code</div>
+                        <a className='hover:text-blue-600' href="https://github.com/CA01971028/tripsite_kbc_it">Git hubコード</a>
                     </div>
                     <div className='text-2xl'>使用言語：フレームワーク</div>
                     <Tag items={['HTML', 'CSS', 'JavaScript', 'Bootstrap']} />
@@ -58,6 +70,10 @@ const Cards = () => {
                     <div className='flex-1'>
                         　3回生の時に２人チームで作成したもので同じハムスターを接触させることでスコアが上昇していくゲームを作成しました。特徴としては、mysqlでスコアを管理しているためランキングなども表示できる。
                     </div>
+                    <div>
+                        <div className='text-2xl'>code</div>
+                        <a className='hover:text-blue-600' href="https://github.com/CA01971028/fruit_game">Git hubコード</a>
+                    </div>
                     <div className='text-2xl'>使用言語：フレームワーク</div>
                     <Tag items={['HTML', 'CSS', 'TypeScript', 'Python', 'React', 'Material-UI', 'mysql', 'Flask']} />
                 </div>
@@ -70,7 +86,11 @@ const Cards = () => {
                         <span>THREAD</span>
                     </div>
                     <div className='flex-1'>
-                        　3回生の時に作成したものでスレッド形式のwebアプリ特徴としては各スレッドに対してコメントをすることができ、新しいスレッドを追加することもできる
+                        　3回生の時に作成したものでスレッド形式のwebアプリ特徴としては各スレッドに対してコメントをすることができ、新しいスレッドを追加することもできる。
+                    </div>
+                    <div>
+                        <div className='text-2xl'>code</div>
+                        <a className='hover:text-blue-600' href="https://github.com/CA01971028/thread">Git hubコード</a>
                     </div>
                     <div className='text-2xl'>使用言語：フレームワーク</div>
                     <Tag items={['HTML', 'CSS', 'TypeScript',  'React', 'Tailwind CSS','Azure']} />
@@ -84,6 +104,11 @@ const Cards = () => {
                     </div>
                     <div className='flex-1'>
                         　3回生の時に作成した作品。３人チームで作成し、私はバックエンド・フロントエンドのフルスタックとしての開発を担当した。ログインでのセッション機能やいいね機能を導入がポイント。また、Azureを利用した作品でもあり、クラウド技術にも精通している。
+                    </div>
+                    <div>
+                        <div className='text-2xl'>code</div>
+                        <a className='hover:text-blue-600' href="https://github.com/CA01971028/qiita_frontend_dep">Git hubコード(next.js)</a><br />
+                        <a className='hover:text-blue-600' href="https://github.com/CA01971028/qiita_frontend_dep">Git hubコード(flask)</a>
                     </div>
                     <div className='text-2xl'>使用言語：フレームワーク</div>
                     <Tag items={['HTML', 'CSS', 'TypeScript',  'Next.js','Flask', 'Tailwind CSS','Azure','Vercel']} />
